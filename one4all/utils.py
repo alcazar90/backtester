42154c2fb1bfc0abdd30e4fe87271878ef9cb3dd
Cristóbal Alcázar <alcazar.cristobal90@gmail.com>
2021-11-05 21:08:15 -0300

    Add modules

--- one4all/utils.py
+++ one4all/utils.py
@@ -3,24 +3,29 @@ utils.py
 
 This file contain utility functions.
 """
+from collections.abc import Mapping, Iterable
+from functools import partial, reduce
+from itertools import product
+import operator
+
 
 def spawn_strategy(cls, parameter_grid):
-    """
+ """
     Given a collection of parameters set; create a dictionary with the given cls instantiated
-    with every set. Create an identificator as key given 'A' + id number from A0, A1, ..., A999
+    with every set. Create an ID as key given 'A' + id number from A0, A1, ..., A999
     :param cls: give a strategy classes (e.g. DCA) with a default set of parameters
     :param parameter_grid:
     :return: A dict with instantiation cls given the ParameterGrid
-    """
+  """
   spawn = [cls(**param) for param in parameter_grid]
   return {'A'+str(i):x for i, x in enumerate(spawn)}
 
 
 class ParameterGrid:
-    """
-    Minimal functionality based on Scikit-learn ParameterGrid class
+  """
+    Minimal functionality based on Scikit-learn h class
     - dic: parameter name as key and as value a list of candidate value
-    """
+  """
   def __init__(self, param_grid):
     if isinstance(param_grid, Mapping):
       # support either dict or list of dicts
@@ -62,5 +67,3 @@ class ParameterGrid:
     product = partial(reduce, operator.mul)
     return sum(product(len(v) for v in p.values()) if p else 1
                 for p in self.param_grid)
-
-
